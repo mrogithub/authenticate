d0406eed9a51fcfdca868ade74dfa6f174d4df92
Michael Roeper <michael.roeper@t-online.de>
2018-05-01 18:25:56 +0200

    adding components and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes} from "@angular/router";
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { LogInComponent } from './components/log-in/log-in.component';
 
+
+const routes: Routes =[{path: 'log-in', component: LogInComponent},
+    { path: 'sign-up', component: SignUpComponent},
+    { path: '', component: LandingComponent},
+    { path: '*', redirectTo: '/'}];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    SignUpComponent,
+    LogInComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+      RouterModule.forRoot(routes)
+          ],
   providers: [],
   bootstrap: [AppComponent]
 })
