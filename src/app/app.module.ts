7db8f38d7aaf010a8d75c7aa3f2d45c2fc8bdbf5
Michael Roeper <michael.roeper@t-online.de>
2018-05-21 18:28:14 +0200

    add an basic service for authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from "@angular/router";
 import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { LogInComponent } from './components/log-in/log-in.component';
+import {AuthService} from "./services/auth.service";
 
 const routes: Routes =[{path: 'log-in', component: LogInComponent},
     { path: 'sign-up', component: SignUpComponent},
@@ -23,9 +25,10 @@ const routes: Routes =[{path: 'log-in', component: LogInComponent},
   imports: [
     BrowserModule,
       FormsModule,
+      HttpClientModule,
       RouterModule.forRoot(routes)
           ],
-  providers: [],
+  providers: [AuthService],        // Bereitstellung eines eigenen Services
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,27 @@
+import {HttpClient} from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import {Observable} from "rxjs/Observable";
+
+import {User} from "../models/user";
+
+@Injectable()
+export class AuthService {
+
+  private BASE_URL = 'http://localhost:1337';   // verwendet für den fake.token-api https://github.com/testdrivenio/fake-token-api
+
+  constructor(private http: HttpClient) { }
+
+  getToken() : string {
+    return localStorage.getItem('token')
+  }
+
+  login(email: string, password: string): Observable<any>{
+    const url   = `${this.BASE_URL}/login`;
+    return this.http.post<User>(url, {email, password});
+  }
+
+  signup(email: string, password: string): Observable<any> {
+    const url = `${this.BASE_URL}/register`;
+      return this.http.post<User>(url, {email, password});
+  }
+}
