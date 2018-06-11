c7e8cd583ad5fb25dc5df4982f39f90144411cdc
Michael Roeper <michael.roeper@t-online.de>
2018-06-11 20:47:29 +0200

    adding module effects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from "@angular/router";
 import { FormsModule } from '@angular/forms';
 import {HttpClientModule} from "@angular/common/http";
+import {EffectsModule} from "@ngrx/effects";
 
 import { AppComponent } from './app.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { LogInComponent } from './components/log-in/log-in.component';
 import {AuthService} from "./services/auth.service";
+import {AuthEffects} from "./store/effects/auth.effects";
 
 const routes: Routes =[{path: 'log-in', component: LogInComponent},
     { path: 'sign-up', component: SignUpComponent},
@@ -26,6 +28,7 @@ const routes: Routes =[{path: 'log-in', component: LogInComponent},
     BrowserModule,
       FormsModule,
       HttpClientModule,
+      EffectsModule.forRoot([AuthEffects]),
       RouterModule.forRoot(routes)
           ],
   providers: [AuthService],        // Bereitstellung eines eigenen Services
