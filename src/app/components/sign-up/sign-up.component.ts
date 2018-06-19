adf5fd794fa070c350d777a9273708dc2d572951
roeper <m.roeper@wenzel-elektronik.de>
2018-06-19 16:53:28 +0200

    enabling error messages on login/signup page

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {Store} from '@ngrx/store';
+import { Observable} from 'rxjs/Observable'
 
 import { User } from '../../models/user';
-import {AppState} from "../../store/app.states";
+import {AppState, selectAuthState} from "../../store/app.states";
 import {SignUp} from '../../store/actions/auth.actions'
 
 @Component({
@@ -13,10 +14,17 @@ import {SignUp} from '../../store/actions/auth.actions'
 export class SignUpComponent implements OnInit {
 
     user: User = new User();
+    getState: Observable<any>;
+    errorMessage: string | null;
 
-  constructor(private store: Store<AppState>) { }
+  constructor(private store: Store<AppState>) {
+      this.getState = this.store.select(selectAuthState);
+  }
 
   ngOnInit() {
+      this.getState.subscribe( (state) => {
+          this.errorMessage = state.errorMessage;
+      })
   }
 
     onSubmit(): void {
--- src/app/store/app.states.ts
+++ src/app/store/app.states.ts
@@ -1,3 +1,4 @@
+import { createFeatureSelector} from "@ngrx/store";
 import * as auth from './reducers/auth.reducers';
 
 //This is a map of keys to the inner state types.
@@ -8,3 +9,5 @@ export interface AppState {
 export const reducers = {
     auth: auth.reducer
 };
+
+export const selectAuthState = createFeatureSelector<AppState>('auth')
