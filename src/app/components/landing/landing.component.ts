52a390cefbdc158240a5fb830dfc8a27adb52240
roeper <m.roeper@wenzel-elektronik.de>
2018-06-19 17:09:07 +0200

    updating landing page with state information

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {Store} from '@ngrx/store';
 
-import { AppState} from "../../store/app.states";
+import {AppState, selectAuthState} from "../../store/app.states";
 import { LogOut} from '../../store/actions/auth.actions';
+import {Observable} from "rxjs/Observable";
 
 @Component({
   selector: 'app-landing',
@@ -11,9 +12,21 @@ import { LogOut} from '../../store/actions/auth.actions';
 })
 export class LandingComponent implements OnInit {
 
-  constructor(private store: Store<AppState>){}
+  getState: Observable<any>;
+  isAuthenticated: false;
+  user = null;
+  errorMessage = null;
+
+  constructor(private store: Store<AppState>){
+    this.getState = this.store.select(selectAuthState);
+  }
 
   ngOnInit() {
+    this.getState.subscribe( (state) => {
+      this.isAuthenticated = state.isAuthenticated;
+      this.user            = state.user;
+      this.errorMessage    = state.errorMessage;
+    })
   }
 
   logout(): void {
